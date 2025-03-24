2d2d4aefb263f8804021b5a63137398e7cd978b2
sangweon25 <seosangweon1290@gmail.com>
2025-03-24 16:21:45 +0900

    feat: Create UISlot
    Slot 프리팹화
    UIInventory Vertical Scroll View
    시작 시 인벤토리 9칸 자동생성

--- Assets/01.Scripts/UI/Inventory/UIInventory.cs
+++ Assets/01.Scripts/UI/Inventory/UIInventory.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class UIInventory : MonoBehaviour
 {
@@ -10,17 +11,42 @@ public class UIInventory : MonoBehaviour
 
     [Header("Item Slots")]
     [SerializeField] List<UISlot> _slots;
+    [SerializeField] Transform _slotPrefab;
 
     [Header("Button")]
     [SerializeField] Button _btn_Back;
 
+    private UISlot prefab;
+
     private void Start()
     {
+        prefab = Resources.Load<UISlot>("Prefabs/Slot");
         _btn_Back.onClick.AddListener(CloseInventory);
+        InitInventoryUI();
+        UpdateCurCapacity();
     }
 
     public void CloseInventory()
     {
         this.gameObject.SetActive(false);
+    }
+
+    public void InitInventoryUI()
+    {
+        if (_slots.Count < 9)
+        {
+            for (int i = _slots.Count; i < 9; i++)
+            {
+                _slots.Add(Instantiate(prefab));
+                _slots[i].transform.SetParent(_slotPrefab);
+                _slots[i].name = $"Slot ({i})";
+
+            }
+        }
+    }
+    
+    private void UpdateCurCapacity()
+    {
+        _txt_CurCapacity.text = _slots.Count.ToString();
     }
 }
